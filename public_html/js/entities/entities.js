0fcd1a077e603f425956ea2d15ab84047b97b4cd
kristopher jones <grofernioken@gmail.com>
2014-11-20 14:02:42 -0800

    this is marioo

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -10,10 +10,13 @@ game.PlayerEntity = me.Entity.extend({
                return (new me.Rect(0, 0, 128, 128)).toPolygon();
            }
        }]);
+       this.renderable.addAnimation("idle", [3]);
        this.body.setVelocity(5, 20);
+       me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
    },
+   
    update:function(delta) {
-        if(me.isKeyPressed("right")){
+        if(me.input.isKeyPressed("right")){
             this.body.vel.x += this.body.accel.x * me.timer.tick;
         }else{
             this.body.vel.x = 0;
--- public_html/js/game.js
+++ public_html/js/game.js
@@ -38,7 +38,7 @@ var game = {
 
 	// Run on game resources loaded.
 	"loaded" : function () {
-                me.pool.register("mario", game.PlayerEnitity, true);
+                me.pool.register("mario", game.PlayerEntity, true);
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
